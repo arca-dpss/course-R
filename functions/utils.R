a299397c3a634ffb15ecf7b8952e66061c9631df
Filippo <filippo.gambarota@gmail.com>
2021-07-19 09:32:57 +0300

    aadding slides template and functions

---
+++ functions/utils.R
@@ -0,0 +1,13 @@
+# This function create a new folder for a new lecture
+
+create_new_lecture <- function(folder_name, slides_name = folder_name, title){
+    
+    template_folder <- "slides/0_template"
+    new_folder <- file.path("slides", folder_name)
+    fs::dir_copy(template_folder, new_folder)
+    
+    # Renaming
+    
+    slides_name <- paste0(slides_name, ".Rmd")
+    file.rename(file.path(new_folder, "template.Rmd"), file.path(new_folder, slides_name))
+}
